f7fe205fc71d4c261fa8a05449afb77693530d0a
Nathan DEUIL <nathan-deuil@outlook.com>
2022-11-28 16:34:48 +0100

    Repair Image affichage

--- public/scripts/produits.js
+++ public/scripts/produits.js
@@ -1,11 +1,11 @@
 const btn = document.getElementById('btn_ajout');
 
 btn.addEventListener('click', () => {
-    const quantite = document.getElementById('product_quantite').value;
+    const quantite = document.getElementById('product_quantite');
     const nom = document.getElementById('product_name').innerText;
     const prix = document.getElementById('product_price').innerText;
-    const id = document.getElementById('product_idr').value;
+    const id = document.getElementById('product_id').value;
 
-    console.log(id);
+    console.log({"id":id,"article":nom,"quantite":quantite.value,"prix":prix});
 
 })
